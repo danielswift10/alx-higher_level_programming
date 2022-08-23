924dd1787616a0e40891e7c9ddd99fc0b1f4ad10
Daniel Ojeyomi <73875730+danielswift10@users.noreply.github.com>
2022-08-23 05:37:06 -0700

    alphab

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(97, 123):
-        if chr(alphabet):
-                 print(f"{alphabet:c}", end='')
+for ch in range(97, 123):
+        print("{:c}".format(ch), end='')
