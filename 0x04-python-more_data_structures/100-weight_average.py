5cf0c1a9add5e0400d5b7febce31ef5817befe2f
¨Daniel <ojeyomi.daniel@gmail.com>
2022-08-31 09:09:43 -0700

    py-task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    length = len(my_list)
+    if not isinstance(my_list, list) or length == 0:
+        return (0)
+
+    average = 0
+    size = 0
+    for tupl in my_list:
+        average += (tupl[0] * tupl[1])
+        size += tupl[1]
+    return (average / size)
