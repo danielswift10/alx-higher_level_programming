d6ba9e8ae5dcc1b9f125ed68c07ae3b50285cc65
Daniel Ojeyomi <73875730+danielswift10@users.noreply.github.com>
2022-08-31 09:24:54 -0700

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,6 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     keys_list = list(new_dict.keys())
-
     for i in keys_list:
         new_dict[i] *= 2
     return (new_dict)
