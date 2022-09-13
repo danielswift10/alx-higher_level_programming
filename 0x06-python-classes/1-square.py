e4c8d86064459a2527e335ba4f26d9e034c5fdc5
Daniel Ojeyomi <73875730+danielswift10@users.noreply.github.com>
2022-09-13 12:40:30 -0500

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""
+a module Sqaure that defines a square
+"""
+
+
 class Square:
-    """Class Square that defines a square object
+    """
+    Definition of a Class Square:
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
     """
     def __init__(self, size):
-        """Initialize method that stores the size of the square
-
-        Args:
-            param1 (int): size of the square
         """
-        self.__size = size
+        initializes the square
+        """
+        self.__size = 
